0bc4a89a8c88d2a1e68d7f82afd2cfd5b36e02c6
Youngjae-Joo <user@DESKTOP-VT7VRHI>
2022-10-31 17:42:18 +0900

    1031

--- DataStructure/src/java_1031/producer/consumer/Main.java
+++ DataStructure/src/java_1031/producer/consumer/Main.java
@@ -5,8 +5,8 @@ public class Main {
 		//자원 생성
 		Product product = new Product();
 		//하나의 자원을 이용해서 2개의 스레드를 생성해서 실행
-		new Producer(product).start();;
-		new Customer(product).start();;
+		new Producer(product).start();
+		new Customer(product).start();
 	}
 
 }
