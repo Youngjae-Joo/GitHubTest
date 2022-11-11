f05804afff5107fa192487fb613622e2411b7e7b
user <user@DESKTOP-48B2DLF>
2022-11-11 19:44:40 +0900

    11_11

--- Quiz/src/quiz01/Main.java
+++ Quiz/src/quiz01/Main.java
@@ -1,8 +1,6 @@
 package quiz01;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main{
 	public static void main(String[] args) {
@@ -12,21 +10,84 @@ public class Main{
 		//				String[] sr=s.split(" ");
 		//				System.out.println(Arrays.toString(sr));
 
-		System.out.println(Solution.solution("3 + 4"));
+		System.out.println(Solution.solution("5525","1255"));
 
 	}
 
-	public static class Solution {
-		public static int solution(String my_string) {
-	        int answer = 0;
-	        my_string=my_string.replaceAll(" ", "");
-	        answer = Integer.parseInt(my_string);
-	        return answer;
-	    }
+	//공던지기
+	//	public static class Solution {
+	//		 public int solution(int[] numbers, int k) {
+	//		        int answer = 0;
+	//		        String s ="";
+	//		        for(int i=0;i<numbers.length;i++) {
+	//		        	s+=numbers[i];
+	//		        }
+	//		        String s2="";
+	//		        for(int i=1;i<=k-1;i++) {
+	//		        	s2+=s;
+	//		        }
+	//		        
+	//		        
+	//		        for(int i=0;i<k;i+=2) {
+	//		        	answer=Integer.parseInt(Integer.toString(s2.charAt(1+i)-48));
+	//		        }
+	//		        
+	//		        
+	//		        return answer;
+	//		    }
+	//		}
+
+
+	//			for(int i=0;i<x.length;i++) {
+	//				if(Y.contains(x[i])) {
+	//					Y=Y.replaceFirst(x[i], "");
+	//					z+=x[i];
+	//				}
+	//				
+	//				if(Y.equals("")) {
+	//					break;
+	//				}
+	//			}
+	public class Solution {
+		public static String solution(String X, String Y) {
+			String answer = "";
+			int[] xcount = new int[10];
+			int[] ycount = new int[10];
+
+			for(int i=0;i<X.length();i++) {
+				xcount[X.charAt(i)-48]++;
+			}
+			for(int i=0;i<Y.length();i++) {
+				ycount[Y.charAt(i)-48]++;
+			}
+
+			for(int i=9;i>=0;i--) {
+				String result="";
+				if(ycount[i]!=0&&xcount[i]!=0){
+					result+=i;
+					result=result.repeat(Math.min(xcount[i], ycount[i]));
+				}
+				answer+=result;
+			}
+			if(answer.length()==0) {
+				answer="-1";
+			}else {
+				if(answer.charAt(0)=='0') {
+					answer="0";
+				}
+			}
+			return answer;
+		}
 	}
 
 
-					
+
+
+
+
+
+
+
 
 
 	//
